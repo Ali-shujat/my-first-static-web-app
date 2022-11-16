d4e0072e192a1ae0d8ffd1cfead9a0d0030860fd
Shujat Ali <the.shujat@gmail.com>
2022-11-16 15:01:25 +0100

    fix some error

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import React from 'react';
 function App() {
     const value = 'Hello World';
     return (
-        <div className="App">
-            <div>{value}</div>
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a>
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+        <div>{value}</div>
+        </>
     );
 
 }
