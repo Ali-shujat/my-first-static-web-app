58e58f8c0d6bab9cfb3578841a3d2d74be044aa4
Shujat Ali <the.shujat@gmail.com>
2022-11-16 14:40:06 +0100

    test passing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
+    const value = 'Hello World';
     return (
         <div className="App">
+            <div>{value}</div>
             <header className="App-header">
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
